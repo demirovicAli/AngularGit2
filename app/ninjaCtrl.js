2147fe8d642151ba5e6c9939dfb637ef50e5a78b
Ali <pokerobuka@gmail.com>
2016-11-02 10:57:56 +0100

    prvi komit

---
+++ app/ninjaCtrl.js
@@ -0,0 +1,7 @@
+angular.module('ninja').controller('ninjaCtrl',function($scope){
+$scope.ime = "Ali Demirovic";
+$scope.datum = "18.10.1986";
+$scope.mail	= "Demirovic@gmail.com";
+$scope.skype = "pnnkaznk3";
+
+});
